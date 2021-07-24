51deb6875b3c8a950b9600c0c2e5cd3648c5a054
eliterose8 <86736567+eliterose8@users.noreply.github.com>
2021-07-24 11:23:22 -0500

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export const Home = () => (
+   <div>
+   Night fell across the marina. The last of the sun’s rays painted everything pale yellow before the horizon extinguished them, leaving the world murky gray. The water beyond the dry dock bay was dark, and people left the water’s edge quickly as night fell.
+
+No one wanted to be caught anywhere near the water with the sun’s protection gone. Even when the sun had been higher in the sky, Katsuki had noticed how people stayed far from the water. There was no telling when or where Blood Riot might emerge. Heaven knew he didn’t seem afraid of anything or anyone.
+
+Katsuki sat on the porch of the small bungalow he was renting. He was a good ten meters from the shore, a wide, long beach stretching between him and the water. Aizawa had assured him that the distance was plenty enough to deter Blood Riot. The shark-creature had never before gone onto dry land, and certainly not this far. Still… before that morning, Aizawa hadn’t seen the monster climb onto a boat, either. Katsuki wasn’t entirely convinced he was safe. He couldn’t stop staring at the lapping waves.
+
+The shallow bay probably wouldn’t pose a problem.
+
+He would probably be safe here, far from the water’s edge and from Blood Riot’s sight.
+
+Probably.
+
+He couldn’t say he was entirely convinced. After all, Blood Riot had pulled himself onto a fucking boat. That was no easy feat. The fact that he’d hauled his girth from the water was something to be admired. The damn shark had to weigh almost a thousand pounds--if not more.
+
+He’s strong as fuck, then.
+
+The thought swam around Katsuki’s mind, circling just like a shark would potential prey.
+
+Heh.
+
+Such strength had to be rare. He couldn’t think of anything that large heaving its body from the water and moving as fast as Blood Riot had. Gravity underwater wasn’t a big deal. Beneath the foam and waves, Blood Riot didn’t have to worry about fighting against its pull to catch prey or follow ships. On land, though… he should have struggled. He shouldn’t have been able to move with such agility and speed.
+
+Yet he had.
+
+He’d acted as though being on the ship was almost natural for him.
+
+The sounds of the sea lapping at the sand should have eased his worries. Instead, it drifted into the background, a static-like noise that he could barely register. The events from that morning made him wonder if Blood Riot would be able to drag his body across the beach to where he was hunkering down for a few days.
+
+He could, if he knew I was here.
+
+He knew it deep down. Katsuki wasn’t safe this close to the shore. He could pretend all he wanted, try to believe Aizawa and brush off the danger, but as he stared at the inky waves licking at the sand, his mind raced with the possibility of Blood Riot clawing his way up the beach. No doubt he would leave massive gouges in the damp sand on his way to devour Katsuki whole.
+
+But he doesn’t know I’m here. There’s no fucking way.
+
+That was his only saving grace. He was easily well out of sight from the water. Anything under the swells wouldn’t be able to see him. The angle was just too sharp. It was a small advantage, one he doubted he would be able to keep long. Tonight? Sure. Blood Riot wouldn’t realize he was here. Probably.  
+   </div>
+  ) 
+   
